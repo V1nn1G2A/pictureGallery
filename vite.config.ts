378792491f6fad9a1cffd43f24c784f933047c79
V1nn1G2A <vip.kirya.filippov.2002@gmail.com>
2024-07-15 16:44:32 +0400

    change vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { createHtmlPlugin } from 'vite-plugin-html';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    createHtmlPlugin({
+      minify: true,
+      inject: {
+        data: {
+          title: 'Picture Gallery',
+        },
+      },
+    }),
+  ],
   base: '/pictureGallery/',
 });
