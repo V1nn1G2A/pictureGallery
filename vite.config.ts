17a5ce592c2373ce84f79ed6b5b03a9e2569c6cc
V1nn1G2A <vip.kirya.filippov.2002@gmail.com>
2024-07-15 16:55:22 +0400

    change vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
       inject: {
         data: {
           title: 'Picture Gallery',
-          favicon: '/public/favicon.png',
+          favicon: 'favicon.png',
         },
       },
     }),
